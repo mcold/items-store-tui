56419c2d77737af2278618ed386e136370de4c24
mcold <mholodnuk@gmail.com>
2025-02-17 14:41:54 +0300

    Rebinding
    Changed hotkeys
    Removed trans field

--- src/page_item.go
+++ src/page_item.go
@@ -13,7 +13,6 @@ import (
 
 type pageItemType struct {
 	items      *tview.List
-	trans      *tview.TextArea
 	descrs     *tview.TextArea
 	filterFrm  *tview.Form
 	itemArea   *tview.TextArea
@@ -27,27 +26,32 @@ type pageItemType struct {
 var pageItem pageItemType
 
 func (pageItem *pageItemType) build() {
-	pageItem.items = tview.NewList()
-	pageItem.items.Box.SetBorder(true)
-	pageItem.items.Box.SetTitle("item (alt+q)")
-	pageItem.items.SetBorderPadding(1, 0, 1, 0)
-
-	pageItem.trans = tview.NewTextArea()
-	pageItem.trans.Box.SetBorder(true)
-	pageItem.trans.Box.SetTitle("short (alt+t)")
-	pageItem.trans.SetBorderPadding(1, 1, 1, 0)
-
-	pageItem.descrs = tview.NewTextArea()
-	pageItem.descrs.Box.SetBorder(true)
-	pageItem.descrs.Box.SetTitle("description (alt+w)")
-	pageItem.descrs.SetBorderPadding(1, 0, 1, 0)
 
 	pageItem.filterFrm = tview.NewForm().
-		AddInputField("ITEM", "", 20, nil, nil).
-		AddInputField("DESCR", "", 22, nil, nil)
+		AddInputField("", "", 20, nil, nil)
 
 	pageItem.filterFrm.Box.SetBorder(true)
-	pageItem.filterFrm.Box.SetTitle("find (alt+f)")
+	pageItem.filterFrm.Box.SetTitle("F2").
+		SetTitleAlign(tview.AlignLeft)
+
+	pageItem.items = tview.NewList()
+	pageItem.items.Box.SetBorder(true).
+		SetTitle("F3").
+		SetTitleAlign(tview.AlignLeft).
+		SetBorderPadding(1, 0, 1, 0)
+
+	pageItem.itemArea = tview.NewTextArea()
+	pageItem.itemArea.SetBorder(true).
+		SetTitle("F4").
+		SetTitleAlign(tview.AlignLeft).
+		SetBackgroundColor(tcell.ColorDarkBlue).
+		SetBorderPadding(1, 1, 1, 1)
+
+	pageItem.descrs = tview.NewTextArea()
+	pageItem.descrs.Box.SetBorder(true).
+		SetTitle("F5").
+		SetTitleAlign(tview.AlignLeft).
+		SetBorderPadding(1, 0, 1, 0)
 
 	pageItem.filterFrm.GetFormItem(0).(*tview.InputField).SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Key() == tcell.KeyTab {
@@ -57,7 +61,7 @@ func (pageItem *pageItemType) build() {
 		return event
 	})
 
-	pageItem.filterFrm.GetFormItem(1).(*tview.InputField).SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+	pageItem.filterFrm.GetFormItem(0).(*tview.InputField).SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
 		if event.Key() == tcell.KeyTab {
 			app.SetFocus(pageItem.filterFrm.GetFormItem(0).(*tview.InputField))
 			return nil
@@ -65,15 +69,8 @@ func (pageItem *pageItemType) build() {
 		return event
 	})
 
-	pageItem.itemArea = tview.NewTextArea()
-	pageItem.itemArea.SetBorder(true)
-	pageItem.itemArea.SetTitle("item (alt+e)")
-
-	pageItem.itemArea.SetBackgroundColor(tcell.ColorDarkBlue)
-	pageItem.itemArea.SetBorderPadding(1, 1, 1, 1)
-
 	flexItem := tview.NewFlex().SetDirection(tview.FlexRow).
-		AddItem(pageItem.filterFrm, 8, 0, true).
+		AddItem(pageItem.filterFrm, 5, 0, true).
 		AddItem(pageItem.items, 0, 10, false).
 		AddItem(pageItem.itemArea, 6, 0, false)
 
@@ -98,7 +95,6 @@ func (pageItem *pageItemType) build() {
 			pageItem.items.AddItem(item.String, trans.String, rune(0), func() {})
 
 			pageItem.mIdDescr[int(id.Int64)] = descr.String
-			pageItem.mIdTrans[int(id.Int64)] = trans.String
 			pageItem.mPosId[rowCount-1] = int(id.Int64)
 			rowCount++
 		}
@@ -109,15 +105,14 @@ func (pageItem *pageItemType) build() {
 	})
 
 	flexDescr := tview.NewFlex().
-		AddItem(pageItem.trans, 0, 2, false).
 		AddItem(pageItem.descrs, 0, 15, false).
 		AddItem(pageItem.saveFrm, 0, 1, false)
 
 	flexDescr.SetDirection(tview.FlexRow)
 
 	flexCmplx := tview.NewFlex().SetDirection(tview.FlexColumn).
-		AddItem(flexItem, 0, 1, true).
-		AddItem(flexDescr, 0, 4, false).
+		AddItem(flexItem, 0, 3, true).
+		AddItem(flexDescr, 0, 8, false).
 		SetFullScreen(true)
 
 	pageItem.filterFrm.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
@@ -126,7 +121,6 @@ func (pageItem *pageItemType) build() {
 			refreshItemList()
 			if pageItem.items.GetItemCount() > 0 {
 				pageItem.items.SetCurrentItem(0)
-				pageItem.trans.SetText(pageItem.mIdTrans[pageItem.mPosId[pageItem.items.GetCurrentItem()]], true)
 				pageItem.descrs.SetText(pageItem.mIdDescr[pageItem.mPosId[pageItem.items.GetCurrentItem()]]+"\n", true)
 			}
 			return nil
@@ -138,42 +132,34 @@ func (pageItem *pageItemType) build() {
 	pageItem.items.SetFocusFunc(func() {
 		itemText, _ := pageItem.items.GetItemText(pageItem.items.GetCurrentItem())
 		pageItem.itemArea.SetText(itemText, true)
-		pageItem.trans.SetText(pageItem.mIdTrans[pageItem.mPosId[pageItem.items.GetCurrentItem()]], false)
 		pageItem.descrs.SetText(pageItem.mIdDescr[pageItem.mPosId[pageItem.items.GetCurrentItem()]], false)
 	})
 
 	pageItem.items.SetSelectedBackgroundColor(tcell.ColorGreen)
 
 	flexCmplx.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-		if event.Rune() == 'q' && event.Modifiers() == tcell.ModAlt {
+		if event.Key() == tcell.KeyF1 {
+			application.pages.ShowPage("help")
+			return nil
+		}
+		if event.Key() == tcell.KeyF2 {
+			app.SetFocus(pageItem.filterFrm)
+			return nil
+		}
+		if event.Key() == tcell.KeyF3 {
 			app.SetFocus(pageItem.items)
 			return nil
 		}
-		if event.Rune() == 't' && event.Modifiers() == tcell.ModAlt {
-			app.SetFocus(pageItem.trans)
-
-			return nil
-		}
-		if event.Rune() == 'w' && event.Modifiers() == tcell.ModAlt {
-			app.SetFocus(pageItem.descrs)
-
-			return nil
-		}
-		if event.Rune() == 'e' && event.Modifiers() == tcell.ModAlt {
+		if event.Key() == tcell.KeyF4 {
 			app.SetFocus(pageItem.itemArea)
-
 			return nil
 		}
-		if event.Rune() == 'f' && event.Modifiers() == tcell.ModAlt {
-			app.SetFocus(pageItem.filterFrm)
+		if event.Key() == tcell.KeyF5 {
+			app.SetFocus(pageItem.descrs)
 			return nil
 		}
 		if event.Key() == tcell.KeyInsert && event.Modifiers() == tcell.ModCtrl {
 			application.pages.SwitchToPage("new item")
-			return nil
-		}
-		if event.Key() == tcell.KeyF1 {
-			application.pages.ShowPage("help")
 			return nil
 		}
 		if event.Key() == tcell.KeyEsc {
@@ -226,7 +212,6 @@ func (pageItem *pageItemType) build() {
 			cnt := pageItem.items.GetItemCount()
 			if curId < cnt {
 				pageItem.items.SetCurrentItem(curId)
-				pageItem.trans.SetText(pageItem.mIdTrans[pageItem.mPosId[pageItem.items.GetCurrentItem()]], true)
 				pageItem.descrs.SetText(pageItem.mIdDescr[pageItem.mPosId[pageItem.items.GetCurrentItem()]]+"\n", true)
 			}
 
@@ -240,7 +225,6 @@ func (pageItem *pageItemType) build() {
 				pageItem.items.SetCurrentItem(curId)
 				itemText, _ := pageItem.items.GetItemText(pageItem.items.GetCurrentItem())
 				pageItem.itemArea.SetText(itemText, true)
-				pageItem.trans.SetText(pageItem.mIdTrans[pageItem.mPosId[pageItem.items.GetCurrentItem()]], true)
 				pageItem.descrs.SetText(pageItem.mIdDescr[pageItem.mPosId[pageItem.items.GetCurrentItem()]]+"\n", true)
 			}
 
@@ -250,7 +234,6 @@ func (pageItem *pageItemType) build() {
 			curId := pageItem.items.GetCurrentItem() - 1
 			if curId > -1 {
 				pageItem.items.SetCurrentItem(curId)
-				pageItem.trans.SetText(pageItem.mIdTrans[pageItem.mPosId[pageItem.items.GetCurrentItem()]], true)
 				pageItem.descrs.SetText(pageItem.mIdDescr[pageItem.mPosId[pageItem.items.GetCurrentItem()]]+"\n", true)
 			}
 
@@ -262,7 +245,6 @@ func (pageItem *pageItemType) build() {
 				pageItem.items.SetCurrentItem(curId)
 				itemText, _ := pageItem.items.GetItemText(pageItem.items.GetCurrentItem())
 				pageItem.itemArea.SetText(itemText, true)
-				pageItem.trans.SetText(pageItem.mIdTrans[pageItem.mPosId[pageItem.items.GetCurrentItem()]], true)
 				pageItem.descrs.SetText(pageItem.mIdDescr[pageItem.mPosId[pageItem.items.GetCurrentItem()]]+"\n", true)
 			}
 
@@ -291,36 +273,20 @@ func refreshItemList() {
 	pageItem.curItemPos = pageItem.items.GetCurrentItem()
 
 	itemToken := strings.TrimSpace(pageItem.filterFrm.GetFormItem(0).(*tview.InputField).GetText())
-	descrToken := strings.TrimSpace(pageItem.filterFrm.GetFormItem(1).(*tview.InputField).GetText())
 	var query string
 
 	log.Println("len itemToken: ", len(itemToken))
-	log.Println("len descrToken: ", len(descrToken))
 
 	if len(itemToken) > 0 {
-		if len(descrToken) > 0 {
-			query = "SELECT id, name, trans, descr" +
-				" FROM item" +
-				" WHERE lower(name) like lower('%" + itemToken + "%')" +
-				" and lower(descr) like lower('%" + descrToken + "%')" +
-				" order by name"
-		} else {
-			query = "SELECT id, name, trans, descr" +
-				" FROM item" +
-				" WHERE lower(name) like lower('%" + itemToken + "%')" +
-				" order by name"
-		}
+		query = "SELECT id, name, trans, descr" +
+			" FROM item" +
+			" WHERE lower(name) like lower('%" + itemToken + "%')" +
+			" or lower(descr) like lower('%" + itemToken + "%')" +
+			" order by name"
 	} else {
-		if len(descrToken) > 0 {
-			query = "SELECT id, name, trans, descr" +
-				" FROM item" +
-				" WHERE lower(descr) like lower('%" + descrToken + "%')" +
-				" order by name"
-		} else {
-			query = "SELECT id, name, trans, descr" +
-				" FROM item" +
-				" order by name"
-		}
+		query = "SELECT id, name, trans, descr" +
+			" FROM item" +
+			" order by name"
 	}
 
 	log.Println(query)
@@ -359,8 +325,7 @@ func save() {
 
 	query := "UPDATE item" + "\n" +
 		"SET name = '" + pageItem.itemArea.GetText() + "',\n" +
-		"descr = '" + pageItem.descrs.GetText() + "',\n" +
-		"trans = '" + pageItem.trans.GetText() + "'\n" +
+		"descr = '" + pageItem.descrs.GetText() + "'\n" +
 		"WHERE id = " + strconv.Itoa(pageItem.mPosId[pageItem.items.GetCurrentItem()])
 
 	log.Println(query)
